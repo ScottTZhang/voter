72fa682f1900387beec44971cad2429a4ef6ac90
Ting Zhang <tingzhang2013@gmail.com>
2014-09-29 00:40:17 -0500

    add Survey table to db, set foreign key

--- db.sql
+++ db.sql
@@ -16,3 +16,21 @@ INSERT INTO `Section` (`id`, `name`, `description`, `status`) VALUES (1, 'sec1',
 INSERT INTO `Section` (`id`, `name`, `description`, `status`) VALUES (2, 'sec2', 'desc2', 1);
 INSERT INTO `Section` (`id`, `name`, `description`, `status`) VALUES (3, 'sec3', 'desc3', 1);
 INSERT INTO `Section` (`id`, `name`, `description`, `status`) VALUES (4, 'sec4', '中文', 1);
+
+DROP TABLE IF EXISTS `Survey`;
+CREATE TABLE `Survey` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `title` varchar(140) NOT NULL,
+    `description` varchar(600) NOT NULL,
+    `status` tinyint,
+    `holder` varchar(40) NOT NULL,
+    `sectionId` int,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`sectionId`) REFERENCES `Section`(`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO `Survey` (`id`, `title`, `description`, `status`, `holder`, `sectionId`) VALUES (1, 'how old are you?', 'as you see', 1, 'admin', 4);
+INSERT INTO `Survey` (`id`, `title`, `description`, `status`, `holder`, `sectionId`) VALUES (2, 'how are you?', 'as you see', 1, 'admin', 3);
+INSERT INTO `Survey` (`id`, `title`, `description`, `status`, `holder`, `sectionId`) VALUES (3, 'how much do you want?', 'as you see', 1, 'admin', 2);
+INSERT INTO `Survey` (`id`, `title`, `description`, `status`, `holder`, `sectionId`) VALUES (4, 'where are you from?', 'as you see', 1, 'admin', 1);
